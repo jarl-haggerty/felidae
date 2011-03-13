f7b22a56a8bd0b72b7390f6645ea0b02f21478a3
Jarl Haggerty <jarl@jarl-System-Product-Name.(none)>
2011-03-13 02:20:15 -0700

    back to java

--- src/main/java/org/curious/felidae/Game.java
+++ src/main/java/org/curious/felidae/Game.java
@@ -7,15 +7,6 @@ package felidae;
 
 import java.awt.Color;
 
-import net.java.games.input.Component;
-import net.java.games.input.Controller;
-import net.java.games.input.ControllerEnvironment;
-import net.java.games.input.Event;
-import net.java.games.input.EventQueue;
-
-import org.lwjgl.opengl.Display;
-import org.lwjgl.util.Timer;
-import org.python.core.PyDictionary;
 import felidae.actors.ActorLoader;
 import felidae.actors.Cast;
 import felidae.audio.Audio;
@@ -23,6 +14,9 @@ import felidae.graphics.Graphics;
 import felidae.input.Input;
 import felidae.logging.Logger;
 import felidae.scripting.Scripting;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
 
 /**
  *
@@ -32,14 +26,12 @@ public class Game {
     public static GameState currentState;
     private static boolean running;
 
-    private static Timer timer;
     public static float delta, lastTime, speed;
 
     public static boolean debugging;
     public static String nextState;
 
     public static ActorLoader actorLoader;
-    public static PyDictionary data;
     public static Cast actors;
     public static String title;
     public static float time, FPS;
@@ -62,16 +54,15 @@ public class Game {
         if(!running){
             return;
         }
-        running &= Scripting.initialize();
-        if(!running){
-            return;
-        }
-        //running &= Input.initialize();
-        if(!running){
-            return;
-        }
         debugging = false;
-        data = new PyDictionary();
+    }
+
+    public static class GameUpdater implements ActionListener{
+
+        public void actionPerformed(ActionEvent e) {
+            currentState = currentState.update();
+        }
+
     }
 
     public static void run(String entryPoint){
@@ -79,113 +70,13 @@ public class Game {
             shutdown();
             return;
         }
-
-        timer = new Timer();
-        lastTime = timer.getTime();
-        delta = 0;
-
-        nextState = null;
         currentState = new GameState(entryPoint, actorLoader);
         currentState.initialize();
-        time = 0;
-        FPS = 0;
-        int framesThrown = 0, frames = 0;
-        float d2 = 0;
-        timer.reset();
-        while(running){
-            d2 = timer.getTime();
-            delta = 1f/Display.getDisplayMode().getFrequency();
-            time += d2;
-            Timer.tick();
-            if(d2 == 0){
-                continue;
-            }
-            timer.reset();
-            if(d2 > .1){
-                framesThrown++;
-                frames--;
-                continue;
-            }
-            
-            delta *= speed;
-            
-            Graphics.update();
-	            
-
-            if(Display.isActive()){
-	            Graphics.clear();
-	            
-	            Controller[] controllers = ControllerEnvironment.getDefaultEnvironment().getControllers();
-				if(controllers.length==0) {
-					System.out.println("Found no controllers.");
-					System.exit(0);
-				}
-				
-				for(int i=0;i<controllers.length;i++) {
-					controllers[i].poll();
-					EventQueue queue = controllers[i].getEventQueue();
-					Event event = new Event();
-					while(queue.getNextEvent(event)) {
-						StringBuffer buffer = new StringBuffer(controllers[i].getName());
-						buffer.append(" at ");
-						buffer.append(event.getNanos()).append(", ");
-						Component comp = event.getComponent();
-						buffer.append(comp.getName()).append(" changed to ");
-						float value = event.getValue(); 
-						if(comp.isAnalog()) {
-							buffer.append(value);
-						} else {
-							if(value==1.0f) {
-								buffer.append("On");
-							} else {
-								buffer.append("Off");
-							}
-						}
-					}
-				}
-	            
-	            currentState.update();
-	            currentState.draw();
-	
-	            Scripting.update();
-	
-	            currentState.vocalize();
-	            Display.sync(60);
-            }
-
-            if(Display.isCloseRequested()){
-                running = false;
-            }
-            
-            frames++;
-            if(time >= 1){
-            	FPS = frames/time;
-            	time = 0;
-            	frames = 0;
-            }
-            Graphics.setColor(Color.black);
-            Graphics.drawStringOnView(String.valueOf(System.getProperty("java.version") + ", " + System.getProperty("java.home")), 0, 5);
-            
-            //GameGraphics.drawString(String.valueOf(time), 0, 100);
-            //Graphics.drawString(String.valueOf(framesThrown), 0, 300);
-
-            if(nextState != null){
-                //break;
-                Graphics.clearMemory();
-                Audio.clearMemory();
-                currentState = new GameState(nextState, actorLoader);
-                currentState.initialize();
-                timer.reset();
-                time = 0;
-                nextState = null;
-            }
-        }
-
-        shutdown();
+        gameTimer = new Timer(16, new GameUpdater());
+        graphicsTimer = new Timer(16, new GraphicsUpdater());
     }
 
     public static void shutdown(){
-        Scripting.destroy();
         Graphics.destroy();
         Audio.destroy();
         Logger.destroy();
